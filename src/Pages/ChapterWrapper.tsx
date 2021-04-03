0cbc3de74aa0804ead1d3c9d27fd28f7612b07d8
Konrad Merz <kmerz@hulud.net>
2021-04-03 09:58:07 +0200

    Add eslint and a border

--- src/Pages/ChapterWrapper.tsx
+++ src/Pages/ChapterWrapper.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
 import styled from 'styled-components';
 
+import ColorPalatte from '../ColorPalatte';
+
 const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   margin: 50px;
+  border: 2px solid ${ColorPalatte.font};
+  border-radius: 5px;
+  padding: 10px;
 `;
 
 type Props = {
