8c0658dcfe9d70c4402489ee1037d31fde8772f7
Konrad Merz <konrad@graylog.com>
2021-04-22 11:39:04 +0200

    Fix missing speech group

--- src/Pages/SakuraIsNotFromTokyo.tsx
+++ src/Pages/SakuraIsNotFromTokyo.tsx
@@ -6,18 +6,23 @@ import motokoAsking from './motoko/motoko-is-asking-satoru.svg';
 import satoruAnswer from './motoko/satoru-is-a-japanese-student.svg';
 
 import Speech from '../TextComponent/Speech';
+import SpeechGroup from '../TextComponent/SpeechGroup';
 
 const AreYouAStudent = () => {
   return (
     <ChapterWrapper>
       <Canvas>
-        <Speech target="出身はどちらですか?" kana="しゅしん わ どちらです か?" source="Where are you from?" />
-        <Speech target="いいえ, あなたは東京出身ではありません。" kana="いいえ , あなた は とうきょう しゅっしん で は ありません。"
-                source="No, you are not from Tokyo." />
+        <SpeechGroup>
+          <Speech target="出身はどちらですか?" kana="しゅしん わ どちらです か?" source="Where are you from?" />
+          <Speech target="いいえ, あなたは東京出身ではありません。" kana="いいえ , あなた は とうきょう しゅっしん で は ありません。"
+                  source="No, you are not from Tokyo." />
+        </SpeechGroup>
         <img title="Motoko" src={motokoAsking} alt="Motoko" />
         <img title="Satoru" src={satoruAnswer} alt="Satoru" />
-        <Speech target="東京出身です。" kana="とうきょう しゅsしんです。" source="I am from Tokyo." />
-        <Speech target="はい、京都に住んでいます。" kana="はい、 きょうと に すんでいます。" source="Yes, I live in Kyoto." />
+        <SpeechGroup>
+          <Speech target="東京出身です。" kana="とうきょう しゅsしんです。" source="I am from Tokyo." />
+          <Speech target="はい、京都に住んでいます。" kana="はい、 きょうと に すんでいます。" source="Yes, I live in Kyoto." />
+        </SpeechGroup>
       </Canvas>
     </ChapterWrapper>
   );
